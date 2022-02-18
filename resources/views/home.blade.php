7f20153b38fbcf6ced46979b4e28919bc3c38756
Marili <marilidandara89@gmail.com>
2022-02-18 17:09:31 -0300

    first

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.app')
+@section('conteudo')
+    <div class="d-flex alignt-items-center justify-content-center mb-5">
+        <img src="https://via.placeholder.com/950x350" />
+    </div>
+
+    <div class="container fimdoconteudo">
+        <div class="row">
+            @foreach($produtos as $produto)
+            <div class="col-4">
+                <div class="card w-100">
+                    <img src="{{ $produto->imagem_url }}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $produto->nome }}</h5>
+                        <p class="card-text">{{ $produto->descricao }}</p>
+                        <a href="/produto/{{ $produto->id }}" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
